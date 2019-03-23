7f3e2b43e8bf8ea3536b5e36475008023dfcab02
herohr <hrzhong@stu.xidian.edu.cn>
2019-03-23 17:52:59 +0800

    增加了简单的验证码签名函数

--- oauth_provider/services/code_check.py
+++ oauth_provider/services/code_check.py
@@ -1,6 +1,9 @@
 from oauth_provider.services.auth_code_center import CodeCenter, Code
+# from oauth_provider.models
 
 def code_check(code, client_id):
     center = CodeCenter()
-    if center.check(client_id, code)
-        authorize_product
+    if center.check(client_id, code):
+        return True
+    return False
+
